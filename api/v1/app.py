c1f07345aa9ce9f8a145265f528b50a61ca8066e
yasmineholb <1055@holbertonschool.com>
2020-04-28 16:17:31 +0200

    error

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 from os import getenv
+from flask import make_response
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -11,8 +12,12 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 def teardown(exception):
     """teardown"""
     storage.close()
-
     
+@app.errorhandler(404)
+def not_found(error):
+    """error handler"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 if __name__ == "__main__":
     host_api = getenv("HBNB_API_HOST", "0.0.0.0")
     port_api = getenv("HBNB_API_PORT", "5000")
