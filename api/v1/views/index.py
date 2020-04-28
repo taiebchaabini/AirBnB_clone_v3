1a170f281716c7c5b15679d7d43b0b03ef4ade28
yasmineholb <1055@holbertonschool.com>
2020-04-28 14:02:30 +0200

    fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,7 @@ from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 
-@app_views.route('/status/')
+@app_views.route('/status')
 def status():
     """ return status"""
     my_dict = {"status": "OK"}
