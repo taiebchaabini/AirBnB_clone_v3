efb0122660239180cb06791e8c2dd04fc6fc2ae1
taiebchaabini <taiebchaabini@gmail.com>
2020-04-28 16:20:50 +0000

    Task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,12 @@
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 """
 Generates routes for Blueprint app_views
 """
@@ -11,4 +18,15 @@ def status():
     Return first simple json for route /status
     """
     my_dict = {'status': "OK"}
-    return (jsonify(my_dict))
+    return jsonify(my_dict)
+
+@app_views.route('/stats')
+def stats():
+    """
+    endpoint that retrieves the number of each objects by type
+    """
+    objects = {'amenities': Amenity, 'cities': City, 'places': Place, 'reviews': Review, 'states': State, 'users': User}
+    stats = {}
+    for k, v in objects.items():
+        stats[k] = storage.count(v)
+    return jsonify(stats)
