5cd6ef4d1cf68e4318bcfb9fae1fbc05cc6b5ad0
yasmineholb <1055@holbertonschool.com>
2020-04-28 14:26:27 +0200

    new

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,3 +7,13 @@ def status():
     """ return status"""
     my_dict = {"status": "OK"}
     return jsonify(my_dict)
+
+@app_views.route('/stats')
+def stats():
+    """return stats"""
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
