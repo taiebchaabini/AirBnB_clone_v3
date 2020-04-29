6753dd4289139d358d68665527cc75a6d711acc0
taiebchaabini <taiebchaabini@gmail.com>
2020-04-29 17:04:46 +0000

    Changing method for stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 """
 Generates routes for Blueprint app_views
 """
@@ -22,9 +28,9 @@ def stats():
     endpoint that retrieves the number of each objects by type
     """
     """return stats"""
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)})
