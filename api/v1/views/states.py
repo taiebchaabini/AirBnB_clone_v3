853969b8d6148298c80d3fe5bd56e9c47ffc73e6
taiebchaabini <taiebchaabini@gmail.com>
2020-04-29 00:28:45 +0000

    Task 7 Done, delete method needs correction.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,6 +3,7 @@ from flask import jsonify, abort, request
 from models import storage
 from models import state
 
+
 def do_check_id(state_id):
     """
         If the state_id is not linked to any State object, raise a 404 error
@@ -39,8 +40,39 @@ def do_delete_state(state_id):
     response = {}
     return jsonify(response)
 
-@app_views.route('/states/', defaults={'state_id': None})
-@app_views.route('/states/<state_id>', methods=['GET', 'DELETE'])
+def do_create_state(request):
+    """
+        Creates a state object
+        Return: new state object
+    """
+    try:
+        body_request = request.get_json(silent=True)
+        if (body_request is None):
+            abort(400, 'Not a JSON')
+        state_name = body_request['name']
+    except KeyError:
+        abort(400, 'Missing name')
+    new_state = state.State(name=state_name)
+    storage.new(new_state)
+    storage.save()
+    return jsonify(new_state.to_dict())
+
+def do_update_state(state_id, request):
+    """
+        Updates a State object
+    """
+    get_state = do_check_id(state_id)
+    body_request = request.get_json(silent=True)
+    if (body_request is None):
+        abort(404, 'Not a JSON')
+    for k, v in body_request.items():
+        if (k not in ('id', 'created_at', 'updated_at')):
+            setattr(get_state, k, v)
+    storage.save()
+    return jsonify(get_state.to_dict())
+
+@app_views.route('/states/', methods=['GET', 'POST'], defaults={'state_id': None})
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
 def states(state_id):
     """
         Handle states requests with needed functions
@@ -49,3 +81,7 @@ def states(state_id):
         return do_get_states(state_id)
     elif (request.method == "DELETE"):
         return do_delete_state(state_id)
+    elif (request.method == "POST"):
+        return do_create_state(request), 201
+    elif (request.method == "PUT"):
+        return do_update_state(state_id, request), 200
