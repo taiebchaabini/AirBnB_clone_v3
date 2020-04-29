7f9239a81210d9f03b8e0496b53f6b9b6b7cf00b
yasmineholb <1055@holbertonschool.com>
2020-04-29 15:15:56 +0200

    comm

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,8 +3,10 @@ from api.v1.views import app_views
 from models import storage
 from os import getenv
 from flask import make_response
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, orgins='0.0.0.0')
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
