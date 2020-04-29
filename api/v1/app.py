9e6f8951f6b5f2101ed6b29e1407b09983975ebb
taiebchaabini <taiebchaabini@gmail.com>
2020-04-29 16:34:13 +0000

    Shebang

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from flask import Blueprint
 """
 Blueprint for app views with url prefix to /api/v1
