3f61cff9b1865f1482a29d3ce50b5a4cdbed9c8b
taiebchaabini <taiebchaabini@gmail.com>
2020-05-05 02:29:48 +0000

    Task 16

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -6,6 +6,10 @@ from models import storage
 from models import city
 from models import place
 from models import user
+from models import state
+from api.v1.views.cities import do_get_cities
+from api.v1.views.places_amenities import do_get_amenities
+import json
 
 
 def do_check_id(cls, place_id):
@@ -90,6 +94,52 @@ def do_update_place(place_id, request):
     return jsonify(get_place.to_dict())
 
 
+def do_search(request):
+    """
+    retrieves all Place objects depending of the JSON
+    in the body of the request
+    """
+    body_request = request.get_json()
+    if body_request is None:
+        abort(400, 'Not a JSON')
+    places_list = []
+    places_amenity_list = []
+    all_cities = []
+    states = body_request.get('states')
+    cities = body_request.get('cities')
+    amenities = body_request.get('amenities')
+    if len(body_request) == 0:
+        places = storage.all(place.Place)
+        for p in places.values():
+            places_list.append(p.to_dict())
+        return jsonify(places_list)
+    if states is not None and len(states) is not 0:
+        for id in states:
+            get_cities = do_get_cities(id, None).json
+            for city in get_cities:
+                all_cities.append(city.get('id'))
+        for id in all_cities:
+            places = do_get_places(id, None)
+            for p in places.json:
+                places_list.append(p)
+    if cities is not None and len(cities) is not 0:
+        for id in cities:
+            places = do_get_places(id, None)
+            for p in places.json:
+                places_list.append(p)
+    if amenities is not None and len(amenities) is not 0:
+        for p in places_list:
+            place_id = p.get('id')
+            get_amenities = do_get_amenities(place_id).json
+            for a in get_amenities:
+                amenity_id = a.get('id')
+                if(amenity_id in amenities):
+                    if p not in places_amenity_list:
+                        places_amenity_list.append(p)
+        return jsonify(places_amenity_list)
+    return jsonify(places_list)
+
+
 @app_views.route('/cities/<city_id>/places/', methods=['GET', 'POST'],
                  defaults={'place_id': None}, strict_slashes=False)
 @app_views.route('/places/<place_id>', defaults={'city_id': None},
@@ -106,3 +156,13 @@ def places(city_id, place_id):
         return do_create_place(request, city_id), 201
     elif (request.method == "PUT"):
         return do_update_place(place_id, request), 200
+
+
+@app_views.route('/places_search', methods=['POST'],
+                 strict_slashes=False)
+def places_search():
+    """
+    retrieves all Place objects depending of the JSON
+    in the body of the request
+    """
+    return do_search(request)
